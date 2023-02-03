aaee2932b93fd4e501ac73867d6ed9602754fd4f
Jameson Taylor <jameson@ionic.io>
2023-02-03 15:03:23 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'broken-shared-header',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
